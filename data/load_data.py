f74a9a68d2673387ad550467cbb4732087d90341
Martamasramon <Marta.masramon@gmail.com>
2024-06-12 15:06:44 +0100

    update load_grad to read as numpy and check if file exists

--- data/load_data.py
+++ data/load_data.py
@@ -4,10 +4,14 @@ import torch
 def load_grad(grad_filename): 
     #TO DO: replace with something that finds the file e.g. pkg_resources.resource_filename
     #grad_files_path = '/Users/paddyslator/python/self-qmri/data'
-
-    grad = torch.tensor(np.loadtxt(grad_filename), dtype=torch.float32)  
+    try:
+        #grad = torch.tensor(np.loadtxt(grad_filename), dtype=torch.float32)  
+        grad = np.loadtxt(grad_filename)  
+        return grad
+    except:
+        return None
     
-    return grad
+    
 
 
 # def load_bvals_bvecs_files(bvals_filename, bvecs_filename):
