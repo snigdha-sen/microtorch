2ad8b8db359be6daf3073dc1924ac5cd5f53ba7e
Martamasramon <Marta.masramon@gmail.com>
2024-06-12 14:35:53 +0100

    Read delta/smalldelta from text file

--- fit.py
+++ fit.py
@@ -22,24 +22,24 @@ def main():
     parser = argparse.ArgumentParser()
     #parser.add_argument("-ld", "--logdir", help="Path to save output", default=f"/tmp/{getpass.getuser()}")
     #parser.add_argument("-lm", "--log_measures", help="Save measures for each epoch", action='store_true')
-    parser.add_argument("-ni", "--num_iters", help="Number of iterations to train for", type=int, default=2000)
-    parser.add_argument("-lr", "--learning_rate", help="Learning rate", type=float, default=3e-4)
-    parser.add_argument("-se", "--seed", help="Random seed", type=int, default=random.randint(1, int(1e6)))
-    parser.add_argument("-img", "--image", help="Filename of the image to train on", default=f"/tmp/{getpass.getuser()}")
-    parser.add_argument("-ma", "--mask", help="Filename of the mask to apply to image", default=f"/tmp/{getpass.getuser()}")
+    parser.add_argument("-ni",  "--num_iters",  help="Number of iterations to train for", type=int, default=2000)
+    parser.add_argument("-lr",  "--learning_rate", help="Learning rate", type=float, default=3e-4)
+    parser.add_argument("-se",  "--seed",       help="Random seed", type=int, default=random.randint(1, int(1e6)))
+    parser.add_argument("-img", "--image",      help="Filename of the image to train on", default=f"/tmp/{getpass.getuser()}")
+    parser.add_argument("-ma",  "--mask",       help="Filename of the mask to apply to image", default=f"/tmp/{getpass.getuser()}")
     parser.add_argument("-lss", "--layer_size", help="Layer sizes as list of ints", type=int, default=256)
-    parser.add_argument("-nl", "--num_layers", help="Number of layers", type=int, default=3)
-    parser.add_argument("-m", "--model", type=str, help="Compartmental Model to use. Implemented are verdict, sandi, or user defined ones form combinations of ball; sphere, stick; astrosticks; cylinder; astrocylinders; zeppelin; astrozeppelins; dot.", default="verdict")
-    parser.add_argument("-a", "--activation", type=str, help="Activation function to use with mlp: elu, relu, prelu or tanh.", default="prelu")
-    parser.add_argument("-op", "--operation", help="Operation to perform (train+fit, train, fit).", default="train+fit")
-    parser.add_argument("-bvals", "--bvals", help="bval file in FSL format and in [s/mm2]", default="")
-    parser.add_argument("-bvecs", "--bvecs", help="bvec file in FSL format", default="")
-    parser.add_argument("-d", "--delta", help="gradient pulse separation in ms", default=24, type=float)
-    parser.add_argument("-sd", "--smalldelta", help="gradient pulse duration in ms", default=8, type=float)
-    parser.add_argument("-TE", "--TE", help="echo time in ms", default="")
-    parser.add_argument("-TR", "--TR", help="repetition time in ms", default="")
-    parser.add_argument("-TI", "--TI", help="inversion time in ms", default="")
-    parser.add_argument("-df","--dropout_frac", help="dropout fraction", type=float, default=0)
+    parser.add_argument("-nl",  "--num_layers", help="Number of layers", type=int, default=3)
+    parser.add_argument("-m",   "--model", type=str, help="Compartmental Model to use. Implemented are verdict, sandi, or user defined ones form combinations of ball; sphere, stick; astrosticks; cylinder; astrocylinders; zeppelin; astrozeppelins; dot.", default="verdict")
+    parser.add_argument("-a",   "--activation", type=str, help="Activation function to use with mlp: elu, relu, prelu or tanh.", default="prelu")
+    parser.add_argument("-op",  "--operation",  help="Operation to perform (train+fit, train, fit).", default="train+fit")
+    parser.add_argument("-bvals", "--bvals",    help="bval file in FSL format and in [s/mm2]", default="")
+    parser.add_argument("-bvecs", "--bvecs",    help="bvec file in FSL format", default="")
+    parser.add_argument("-d",   "--delta",      help="txt file with gradient pulse separation (ms)", default="data/delta.txt",      type=str)
+    parser.add_argument("-sd",  "--smalldelta", help="txt file with gradient pulse duration (ms)",   default="data/smalldelta.txt", type=str)
+    parser.add_argument("-TE",  "--TE",         help="echo time in ms", default="")
+    parser.add_argument("-TR",  "--TR",         help="repetition time in ms", default="")
+    parser.add_argument("-TI",  "--TI",         help="inversion time in ms", default="")
+    parser.add_argument("-df",  "--dropout_frac", help="dropout fraction", type=float, default=0)
 
     args = parser.parse_args()
     mlp_activation = {'relu': torch.nn.ReLU(),'prelu': torch.nn.PReLU, 'tanh': torch.nn.Tanh(), 'elu': torch.nn.ELU()}
@@ -94,8 +94,12 @@ def main():
 
     def grad_maker(bvals, bvecs, delta, smalldel):
 
+        # Load values from text files
         bvals = np.loadtxt(bvals)
         bvecs = np.loadtxt(bvecs)
+        delta = np.loadtxt(delta)
+        smalldel = np.loadtxt(smalldel)
+        
         bvals = bvals * 1e-3 #in ms/um2
         bvecs = np.transpose(bvecs)
         gamma = 2.67e2 #ms^-1mT-1
