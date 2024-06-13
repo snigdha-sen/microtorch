a5bf3727750563be053c8309d398cb97be78082f
Snigdha Sen <snigdhasen98@gmail.com>
2024-06-13 15:14:29 +0100

    changes to readme

--- signal_models.py
+++ signal_models.py
@@ -111,7 +111,7 @@ class Sphere:
         alpha = alpha.unsqueeze(1)
         alpha2 = alpha2.unsqueeze(1)
         alpha2D = alpha2D.unsqueeze(1)
-
+ 
         gamma = 2.675987e2
         gradient_strength   = torch.FloatTensor([np.sqrt(b_values[i])/(gamma*delta[i]*np.sqrt(Delta[i]-delta[i]/3)) for i,_ in enumerate(b_values)]) 
         first_factor        = -2*(gamma*gradient_strength)**2 / 2
