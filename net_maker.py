cbe515895182e0a891c3c1656af32e08c692b50f
garends3 <G.C.Arends-3@umcutrecht.nl>
2024-06-14 17:42:24 +0200

    small debugs transpos

--- net_maker.py
+++ net_maker.py
@@ -1,22 +1,11 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import signal_models
 
-# WRAPPER FUNCTION MAKE NETWORK 
-# make_net((grad, model, dim_hidden, dim_out, num_layers, dropout_frac, activation=nn.PReLU()))
-#
-# grad: gradient table/sequence details
-# model: compartmental model to fit
-# dim_hidden: number of units in each hidden layer
-# num_layers: number of hidden layers
-# dropout_frac: dropout fraction
-# activation: activation function for each layer
-
-#def net_maker(grad, modelfunc, dim_hidden, num_layers, dropout_frac, activation=nn.PReLU()):
 class Net(nn.Module):
+
     def __init__(self,grad, modelfunc, dim_hidden, num_layers, dropout_frac, clipping_method = 'clamp', activation=nn.PReLU()):  
+
         """
         Define the network architecture
         
@@ -31,9 +20,9 @@ class Net(nn.Module):
         
         super(Net, self).__init__()
         self.grad = grad
-        self.grad = grad
         self.modelfunc  = modelfunc
         self.clipping_method = clipping_method
+
         dim_in          = dim_hidden
         self.fc_layers  = nn.ModuleList()
         self.fc_layers.extend([nn.Linear(dim_in, dim_hidden), activation])
@@ -58,21 +47,16 @@ class Net(nn.Module):
             X = self.dropout(X)
 
         #params = self.encoder(X)              
-        #params = self.encoder(X)              
         params = F.softplus(self.encoder(X))
         #params = abs(self.encoder(X))
-        
+        #get the signal model function        
+        #modelfunc = getattr(models, model)
         modelfunc = self.modelfunc
         clipping_method = self.clipping_method
                                
         for i in range(modelfunc.n_params): #set min/max of non-volume fraction parameters       
             params[:,i] = Net.squash(params[:, i].clone().unsqueeze(1), clipping_method, modelfunc.parameter_ranges[i,0], modelfunc.parameter_ranges[i,1])
-        clipping_method = self.clipping_method
-                               
-        for i in range(modelfunc.n_params): #set min/max of non-volume fraction parameters       
-            params[:,i] = Net.squash(params[:, i].clone().unsqueeze(1), clipping_method, modelfunc.parameter_ranges[i,0], modelfunc.parameter_ranges[i,1])
          
-        #set min/max of volume fraction parameters  
         #set min/max of volume fraction parameters  
         for i in range(modelfunc.n_params, modelfunc.n_params + modelfunc.n_frac):
             # Set negative values to 0
@@ -81,12 +65,7 @@ class Net(nn.Module):
         sum_params = torch.sum(params[:, modelfunc.n_params:modelfunc.n_params + modelfunc.n_frac], dim=1, keepdim=True)
         for i in range(modelfunc.n_params, modelfunc.n_params + modelfunc.n_frac): #set min/max of volume fraction parameters  
             # Normalize to make the sum of params[:, i] equal to 1
-        for i in range(modelfunc.n_params, modelfunc.n_params + modelfunc.n_frac): #set min/max of volume fraction parameters  
-            # Normalize to make the sum of params[:, i] equal to 1
             params[:, modelfunc.n_params:modelfunc.n_params + modelfunc.n_frac] /= sum_params
-
-
-        
 
 
         
@@ -112,26 +91,3 @@ class Net(nn.Module):
             raise ValueError("Unsupported method: {}".format(method))
 
         return unsqueezed_param
-
-
-    
-
-
-    def squash(param, method, p_min, p_max):
-
-        if method == 'clamp':
-
-            squashed_param_tensor =torch.clamp(param, min=p_min, max=p_max)
-            unsqueezed_param = squashed_param_tensor.squeeze(1)
-
-        elif method == 'sigmoid':
-
-            sigmoid_param = torch.sigmoid(param)
-            scaled_param = p_min + (p_max - p_min) * sigmoid_param
-            unsqueezed_param = scaled_param.squeeze(1)
-
-        else:
-            raise ValueError("Unsupported method: {}".format(method))
-
-        return unsqueezed_param
-
