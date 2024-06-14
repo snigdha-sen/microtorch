ff615c37e1ce59eaf5d39450490ca1895abd27cc
Martamasramon <Marta.masramon@gmail.com>
2024-06-14 12:13:18 +0100

    clean code

--- net_maker.py
+++ net_maker.py
@@ -1,36 +1,37 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import signal_models
 
-# WRAPPER FUNCTION MAKE NETWORK 
-# make_net((grad, model, dim_hidden, dim_out, num_layers, dropout_frac, activation=nn.PReLU()))
-#
-# grad: gradient table/sequence details
-# model: compartmental model to fit
-# dim_hidden: number of units in each hidden layer
-# num_layers: number of hidden layers
-# dropout_frac: dropout fraction
-# activation: activation function for each layer
-
-#def net_maker(grad, modelfunc, dim_hidden, num_layers, dropout_frac, activation=nn.PReLU()):
 class Net(nn.Module):
     def __init__(self, grad, modelfunc, dim_hidden, num_layers, dropout_frac, activation=nn.PReLU()):  
+        """
+        Define the network architecture
+        
+        Args:
+            grad: gradient table/sequence details
+            model: compartmental model to fit
+            dim_hidden: number of units in each hidden layer
+            num_layers: number of hidden layers
+            dropout_frac: dropout fraction
+            activation: activation function for each layer. Defaults to nn.PReLU().
+        """
+        
         super(Net, self).__init__()
-        #add gradient table
-        self.grad = grad
-        self.modelfunc = modelfunc
-        dim_in = dim_hidden
-        self.fc_layers = nn.ModuleList()
-        #create the first layer - input layer
+        self.grad       = grad
+        self.modelfunc  = modelfunc
+        dim_in          = dim_hidden
+        self.fc_layers  = nn.ModuleList()
         self.fc_layers.extend([nn.Linear(dim_in, dim_hidden), activation])
-        #get the number of signal model parameters
+        
+        # Get the number of signal model parameters
         dim_out = modelfunc.n_params + modelfunc.n_frac
         
-        for i in range(num_layers-1): # num_layers fully connected hidden layers - number of nodes defined by the user as dim_hidden
+        # Add fully connected hidden layers 
+        for _ in range(num_layers-1): 
             self.fc_layers.extend([nn.Linear(dim_hidden, dim_hidden), activation])
-        self.encoder = nn.Sequential(*self.fc_layers, nn.Linear(dim_hidden, dim_out)) # Add the last linear layer for regression
+        
+        # Add the last linear layer for regression    
+        self.encoder = nn.Sequential(*self.fc_layers, nn.Linear(dim_hidden, dim_out)) 
         
         self.dropout_frac = dropout_frac
         if dropout_frac > 0:
@@ -41,32 +42,26 @@ class Net(nn.Module):
         if self.dropout_frac > 0:
             X = self.dropout(X)
 
-        #params = self.encoder(X)   
         params = F.softplus(self.encoder(X))
               
-        #get the signal model function        
-        #modelfunc = getattr(models, model)
+        # Get the signal model function        
         modelfunc = self.modelfunc
-                               
-        for i in range(modelfunc.n_params): #set min/max of non-volume fraction parameters       
+                       
+        # Set min/max of non-volume fraction parameters               
+        for i in range(modelfunc.n_params): 
             this_param_clamped = torch.clamp(params[:, i].clone().unsqueeze(1), min = modelfunc.parameter_ranges[i,0], max =  modelfunc.parameter_ranges[i,1])  
             params[:,i] = this_param_clamped.squeeze()
          
-        #set min/max of volume fraction parameters  
+        # Set min/max of volume fraction parameters  
         for i in range(modelfunc.n_params, modelfunc.n_params + modelfunc.n_frac):
             # Set negative values to 0
             params[:, i] = torch.relu(params[:, i]) 
 
+        # Normalize to make the sum of params[:, i] equal to 1
         sum_params = torch.sum(params[:, modelfunc.n_params:modelfunc.n_params + modelfunc.n_frac], dim=1, keepdim=True)
-        for i in range(modelfunc.n_params, modelfunc.n_params + modelfunc.n_frac): #set min/max of volume fraction parameters  
-            # Normalize to make the sum of params[:, i] equal to 1
+        for i in range(modelfunc.n_params, modelfunc.n_params + modelfunc.n_frac):
             params[:, modelfunc.n_params:modelfunc.n_params + modelfunc.n_frac] /= sum_params
 
-
-        
         X = self.modelfunc(self.grad, params)
 
-        
-        
         return X.to(torch.float32), params
-    #return Net(grad, modelfunc, dim_hidden, num_layers, dropout_frac, activation=nn.PReLU())
