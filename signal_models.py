f08c59245b1cc1a5e28c4d0d6828556feb3374eb
Snigdha Sen <snigdhasen98@gmail.com>
2024-06-14 10:06:44 +0100

    changes to cylinder

--- signal_models.py
+++ signal_models.py
@@ -260,7 +260,7 @@ class t1_smdt:
         S = sfac * S0 * torch.abs(1.0 - torch.exp(-TI/T1) - (torch.exp(-TS/T1)) * torch.exp(-TI/T1)) * torch.erf(torch.sqrt(b_values*(Dpar-Dperp)))/torch.sqrt(b_values*(Dpar-Dperp))
 
         return S
-'''   
+   
     class Cylinder:
 
         def __init__(self, grad, params):
@@ -271,8 +271,9 @@ class t1_smdt:
             self.spherical_mean = False
 
         def __call__(self, grad, params):
+            
+            
 
-'''
 
 
 
