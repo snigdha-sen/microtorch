1b1890c1563138bbe2111c0b7f2674a7da5b8106
snigdha-sen <snigdhasen98@gmail.com>
2026-01-30 15:09:41 +0000

    add plotting to main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 from src.run_fit import run_fit
+from src.plot_results import plot_param_maps
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(cfg: DictConfig):
@@ -13,7 +14,12 @@ def main(cfg: DictConfig):
     print("Running with config:\n")
     print(OmegaConf.to_yaml(cfg))
 
-    run_fit(cfg)
+    _, modelfunc, out_file = run_fit(cfg)
+
+    if cfg.plot.enabled:
+        plot_param_maps(out_file, modelfunc, zslice=cfg.plot.zslice)
+
 
 if __name__ == "__main__":
     main()
+
