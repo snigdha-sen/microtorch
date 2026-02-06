1a6d3587ffdaf656b626ce5beb468f16033ddabd
snigdha-sen <snigdhasen98@gmail.com>
2026-02-06 11:17:03 +0000

    refactor acquisition scheme and add tests

--- src/utils/load_data.py
+++ src/utils/load_data.py
@@ -6,13 +6,13 @@ def load_grad(grad_filename):
     #grad_files_path = '/Users/paddyslator/python/self-qmri/data'
     try:
         #grad = torch.tensor(np.loadtxt(grad_filename), dtype=torch.float32)  
-        print(grad_filename)
+        #print(grad_filename)
         grad = np.loadtxt(grad_filename)
         
         if len(grad.shape) < 2:
             grad = grad[:,None]
         
         return grad #np.transpose(grad)
-    except:
+    except OSError:
         return None
 
