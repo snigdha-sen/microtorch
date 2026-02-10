b1bcdbd4784cad9a00bc90b4cd9ee2549f4b68d3
snigdha-sen <snigdhasen98@gmail.com>
2026-02-10 14:34:17 +0000

    tidy model_maker.py

--- src/utils/plot_results.py
+++ src/utils/plot_results.py
@@ -5,14 +5,28 @@ def plot_param_maps(nifti_file, modelfunc, zslice=0):
     img = nib.load(nifti_file).get_fdata()
 
     n_maps = img.shape[-1]
-    _, ax = plt.subplots(1, n_maps, figsize=(5 * n_maps, 2))
+    fig, ax = plt.subplots(1, n_maps, figsize=(5 * n_maps, 2))
+    if n_maps == 1:
+        ax = [ax]  # make iterable
 
     for i in range(n_maps):
         im = ax[i].imshow(img[:, :, zslice, i])
         plt.colorbar(im, ax=ax[i])
-        ax[i].set_title(
-            f"{modelfunc.parameter_names[i]} "
-            f"({modelfunc.compartment_names[modelfunc.compartment_indices[i]]})"
-        )
 
+        if i < modelfunc.n_parameters:
+            cp_idx = next(
+                (j for j, slc in enumerate(modelfunc.parameter_slices)
+                 if slc.start <= i < slc.stop),
+                None
+            )
+            cp_label = modelfunc.compartment_names[cp_idx] if cp_idx is not None else "UnknownCompartment"
+        else:
+            # It's a fraction parameter
+            frac_idx = i - modelfunc.n_parameters
+            cp_label = f"fraction f_{frac_idx}"
+
+        ax[i].set_title(f"{modelfunc.parameter_names[i]} ({cp_label})")
+
+    plt.tight_layout()
     plt.show()
+
